cc8b4d7ec6f57c945f19df16b57a43803582cff1
Valentin Cocaud <v.cocaud@gmail.com>
2022-08-19 23:38:47 +0200

    :bug: fix graphql endpoint

--- packages/frontend/src/graphql-client.js
+++ packages/frontend/src/graphql-client.js
@@ -3,7 +3,7 @@ import { refocusExchange } from '@urql/exchange-refocus';
 
 export const client = createClient({
   // TODO: Add env variables for this
-  url: 'http://localhost:8080/v1/graphql',
+  url: 'https://a-table.caprover.cocaud.dev/v1/graphql',
   exchanges: [dedupExchange, refocusExchange(), cacheExchange, fetchExchange],
   fetchOptions: () => {
     const token = localStorage.getItem('token');
--- packages/frontend/src/routes/login.jsx
+++ packages/frontend/src/routes/login.jsx
@@ -5,7 +5,7 @@ import {AuthScreen} from "../components/auth-screen";
 
 export function LoginScreen() {
   const navigate = useNavigate();
-  const [{fetching, data}, login] = useMutation(/* GraphQL */ `
+  const [{fetching, data, error}, login] = useMutation(/* GraphQL */ `
     mutation login($username: String!, $password: String!) {
       login(args: {username_input: $username, password_input: $password}) {
         token
@@ -27,7 +27,7 @@ export function LoginScreen() {
       <AuthScreen.Title>Login</AuthScreen.Title>
       <Input name="username" placeholder="Utilisateur" required/>
       <Input name="password" type="password" placeholder="Mot de passe" required/>
-      <AuthScreen.Error visible={data && !data?.login?.token}>Mauvais utilisateur ou mot de passe</AuthScreen.Error>
+      <AuthScreen.Error visible={(data && !data?.login?.token) || error}>{error?.message || 'Mauvais utilisateur ou mot de passe'}</AuthScreen.Error>
       <AuthScreen.Link to="/register" >Pas encore inscrit ?</AuthScreen.Link>
       <AuthScreen.Actions okText="Se connecter" fetching={fetching}/>
     </AuthScreen>
